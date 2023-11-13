8f6f4926626ec26199da93b5b8ac5beb2ed7e224
Tran A Huy <you@example.com>
2023-11-13 13:44:19 +0700

    hoanthien

--- ahuy.cpp
+++ ahuy.cpp
@@ -1,10 +1,20 @@
 
 #include <iostream>
 using namespace std;
+bool kiemTraHoanThien(int n) {
+	if((int)sqrt(n)==sqrt(n))
+		return true;
+	return false;
+}
 
 int main() {
 	int n;
 	cout << "Nhap 1 so nguyen n";
 	cin >> n;
+	if (kiemTraHoanThien(n) == 1) {
+		cout << n << " la so hoan thien";
+	}
+	else
+		cout << n << " la so hoan thien";
 	return 0;
 }
