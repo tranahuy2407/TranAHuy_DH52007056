bda35dbb9a4da09132deacf2c8978e736f237be5
Tran A Huy <you@example.com>
2023-11-13 13:38:01 +0700

    chinh phuong

--- ahuy.cpp
+++ ahuy.cpp
@@ -1,10 +1,28 @@
 
 #include <iostream>
 using namespace std;
+bool kiemTraChinhPhuong(int n) {
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (n%i == 0) {
+			s += i;
+		}
+	}
+	if (s == n)
+		return true;
+	return false;
+}
 
 int main() {
 	int n;
 	cout << "Nhap 1 so nguyen n";
 	cin >> n;
+	if (kiemTraChinhPhuong(n) == 1) {
+		cout << n << " la so chinh phuong";
+	}
+	else {
+		cout << n << " khong la so chinh phuong";
+	}
 	return 0;
 }
